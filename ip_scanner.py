90cb1ca2c93f3451a3f344e4731c59302ee77954
garrettvolpe <garrettvolpe@gmail.com>
2022-07-23 21:52:04 -0400

    add IP to hostname lookup

--- ip_scanner.py
+++ ip_scanner.py
@@ -6,19 +6,24 @@ ip_start_range = 1
 ip_end_range = 254
 list_of_ips = []
 online_ips = []
-print(ip_end_range)
 
 while ip_start_range != ip_end_range:
-    print(ip_start_range)
     list_of_ips.append(network + str(ip_start_range))
     ip_start_range += 1
 
 for a in list_of_ips:
-    print(a)
     response = ping (a)
-    if response != False:
+    if response != None:
         print (a)
+        print(response)
         online_ips.append(a)
 
 print(online_ips)
 
+for t in online_ips:
+    try:
+        hostname = socket.gethostbyaddr(t)
+        print(hostname)
+    except:
+        pass
+     # print(hostname[0],hostname[2])
